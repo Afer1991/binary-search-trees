ed7cee088aca892194098fb9fb0ae6686078953c
Af1991 <152735889+Afer1991@users.noreply.github.com>
2024-11-13 11:12:00 -0500

    Build Tree class, add funcs to build bst

--- bst.js
+++ bst.js
@@ -5,3 +5,56 @@ class Node {
     this.right = null;
   }
 }
+
+class Tree {
+  constructor(arr) {
+    this.root = buildTree(sortArray(arr), 0, sortArray(arr).length - 1);
+  }
+
+  print() {
+    prettyPrint(this.root);
+  }
+}
+
+const buildTree = (arr, start, end) => {
+  if (start > end) {
+    return null;
+  } else {
+    const mid = Math.floor((start + end) / 2);
+    const root = new Node(arr[mid]);
+
+    root.left = buildTree(arr, start, mid - 1);
+    root.right = buildTree(arr, mid + 1, end);
+
+    return root;
+  };
+};
+
+const prettyPrint = (node, prefix = "", isLeft = true) => {
+  if (node === null) {
+    return;
+  };
+  if (node.right !== null) {
+    prettyPrint(node.right, `${prefix}${isLeft ? "│   " : "    "}`, false);
+  };
+  console.log(`${prefix}${isLeft ? "└── " : "┌── "}${node.data}`);
+  if (node.left !== null) {
+    prettyPrint(node.left, `${prefix}${isLeft ? "    " : "│   "}`, true);
+  };
+};
+
+const sortArray = (arr) => {
+  const sortedArr = arr.sort((a, b) => {
+    if (a > b) {
+      return 1;
+    } else if (a < b) {
+      return -1;
+    } else {
+      return 0;
+    }
+  });
+
+  const removeDupes = [...new Set(sortedArr)];
+
+  return removeDupes;
+};
